60e0eec555827f94defb458106f7a9d4f919f7be
Your Name <guilherme3688@gmail.com>
2022-01-04 22:20:09 -0300

    First Commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public string InsertPessoa(Pessoa pes)
+        {       
+            try
+            {
+                using (PessoaEntities db = new PessoaEntities())
+                {
+                    db.Pessoas.Add(pes);
+                    db.SaveChanges();
+                    return "Pessoa cadastrada com sucesso!";
+                }
+            }
+            catch (Exception ex)
+            {
+                string ex2 = ex.Message;
+                return ex2;
+               
+            }
+
+        }
+
+        public ActionResult Sobre()
+        {
+            ViewBag.Message = "Autor: Guilherme R P Silva";
+
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication1/obj/Release/Package/PackageTmp/Scripts/Pessoa.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope, $http) {
+    
+    $scope.InsertPessoa = function () {
+        var type = document.getElementById("insertPes").getAttribute("value");
+        if (type == "Enviar") {
+            $http({
+                method: "POST",
+                url: "https://localhost:44390/Home/InsertPessoa?Nome=" + $scope.PNome + "&Idade=" + $scope.PIdade + "&Telefone=" + $scope.PTelefone + "&Email=" + $scope.PEmail,
+                datatype: "JSON",
+            }).then(function (response) {
+                alert(response.data);
+            })
+        }
+    }
+});
